23cfb44e42224f8e60b7c6ef45f2e1f6b9086540
mahchuchu <matthew.hunter08@gmail.com>
2017-11-21 00:28:38 -0800

    fixed infinite jumps and attached camera to player

--- Sunset Chaser/Assets/Scripts/CloudianController.cs
+++ Sunset Chaser/Assets/Scripts/CloudianController.cs
@@ -9,18 +9,29 @@ public class CloudianController : MonoBehaviour {
 
 	private Rigidbody2D myBody;
 
+	public bool grounded;
+	public LayerMask whatIsGround;
+
+	private Collider2D myCollider;
+
 	// Use this for initialization
 	void Start () {
 		myBody = GetComponent<Rigidbody2D>();
+		myCollider = GetComponent<Collider2D>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+
+		grounded = Physics2D.IsTouchingLayers (myCollider, whatIsGround);
+		
 		myBody.velocity = new Vector2 (moveSpeed, myBody.velocity.y);
 
 		if (Input.GetKeyDown (KeyCode.Space)) {
-			myBody.velocity = new Vector2 (myBody.velocity.x, jumpForce);
+			if (grounded) {
+				myBody.velocity = new Vector2 (myBody.velocity.x, jumpForce);
+			}
 		}
 	}
 }
