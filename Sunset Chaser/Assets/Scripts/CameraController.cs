cc69ca79f8e885c2505307dcd802e29484c47273
mahchuchu <matthew.hunter08@gmail.com>
2017-11-21 11:57:55 -0800

    fixed camera, changed cube tile to cloud

---
+++ Sunset Chaser/Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public CloudianController cloudPlayer;
+
+	private Vector3 lastPlayerPosition;
+	private float distanceToMove;
+
+	// Use this for initialization
+	void Start () {
+		cloudPlayer = FindObjectOfType<CloudianController>();
+		lastPlayerPosition = cloudPlayer.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distanceToMove = cloudPlayer.transform.position.x - lastPlayerPosition.x;
+
+		transform.position = new Vector3(transform.position.x + distanceToMove, transform.position.y, transform.position.z);
+
+		lastPlayerPosition = cloudPlayer.transform.position;
+
+	}
+}
