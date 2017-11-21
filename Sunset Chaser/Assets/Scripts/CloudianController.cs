859d81a8907c04f6beccf6366d6006ec69262bdc
mahchuchu <matthew.hunter08@gmail.com>
2017-11-20 23:46:24 -0800

    added player and movement

---
+++ Sunset Chaser/Assets/Scripts/CloudianController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudianController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpForce;
+
+	private Rigidbody2D myBody;
+
+	// Use this for initialization
+	void Start () {
+		myBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		myBody.velocity = new Vector2 (moveSpeed, myBody.velocity.y);
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			myBody.velocity = new Vector2 (myBody.velocity.x, jumpForce);
+		}
+	}
+}
