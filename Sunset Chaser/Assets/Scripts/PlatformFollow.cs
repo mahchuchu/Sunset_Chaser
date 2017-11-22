ffcb143e77ceb76c876134e1021dfefdac814159
mahchuchu <matthew.hunter08@gmail.com>
2017-11-22 02:57:38 -0800

    fixed platform that wouldn't follow main camera

---
+++ Sunset Chaser/Assets/Scripts/PlatformFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFollow : MonoBehaviour {
+
+	private Rigidbody2D plat;
+
+	public float moveSpeed;
+	//public GameObject[] deletePlat;
+
+	// Use this for initialization
+	void Start () {
+		plat = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		plat.velocity = new Vector2 (moveSpeed, plat.velocity.y);
+
+		if (gameObject.name == "cloudP1(Clone)") {
+			Destroy (GetComponent<Rigidbody2D>());
+		}
+
+		/*deletePlat = GameObject.FindGameObjectWithTag ("ground");
+		for (int i = 0; i < deletePlat.Length; i++) {
+			Destroy(GetComponent<Rigidbody2D>());
+		}*/
+
+	}
+}
