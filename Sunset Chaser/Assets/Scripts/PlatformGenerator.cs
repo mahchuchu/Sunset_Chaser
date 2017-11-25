e2b01c42c89171098ae2f21b3da30acdad720f3d
mahchuchu <matthew.hunter08@gmail.com>
2017-11-24 18:13:22 -0800

    randomized platform of different sizes

--- Sunset Chaser/Assets/Scripts/PlatformGenerator.cs
+++ Sunset Chaser/Assets/Scripts/PlatformGenerator.cs
@@ -11,14 +11,27 @@ public class PlatformGenerator : MonoBehaviour {
 
 	private float platformWidth;
 
+	private float[] platformWidths;
+
 	public float distBetweenMin;
 	public float distBetweenMax;
 
-	public ObjectPooler theObjectPool;
+	public GameObject[] thePlatforms;
+	private int platformSelector;
+
+	//public ObjectPooler theObjectPool;
 
 	// Use this for initialization
-	void Start () {
-		platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+	void Start ()
+	{
+		//platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+
+		platformWidths = new float[thePlatforms.Length];
+
+		for (int i = 0; i < thePlatforms.Length; i++) {
+			platformWidths[i] = thePlatforms[i].GetComponent<BoxCollider2D>().size.x;
+		}
+
 	}
 	
 	// Update is called once per frame
@@ -30,18 +43,26 @@ public class PlatformGenerator : MonoBehaviour {
         //Same as above, except this makes the player keep its local orientation rather than its global orientation.
         thePlatform.transform.SetParent(fixedPlat, false);
 
-        distBetween = Random.Range(distBetweenMin, distBetweenMax);
 
 		if (transform.position.x < generationPoint.position.x) {
-			transform.position = new Vector3(transform.position.x + platformWidth + distBetween, transform.position.y, transform.position.z);
-			//Instantiate (thePlatform, transform.position, transform.rotation);
 
+			distBetween = Random.Range(distBetweenMin, distBetweenMax);
+
+			platformSelector = Random.Range(0, thePlatforms.Length);
+
+			transform.position = new Vector3(transform.position.x + platformWidths[platformSelector] + distBetween, transform.position.y, transform.position.z);
+
+
+
+			Instantiate (/*thePlatform */ thePlatforms[platformSelector], transform.position, transform.rotation);
+
+			/*
 			GameObject newPlatform = theObjectPool.GetPooledObject();
 
 			newPlatform.transform.position = transform.position;
 			newPlatform.transform.rotation = transform.rotation;
 			newPlatform.SetActive (true);
-
+			*/
 		}
 	}
 }
