b470903f7a7f6d44c69cad58a18dec7ce045da67
mahchuchu <matthew.hunter08@gmail.com>
2017-11-26 01:20:13 -0800

    cleaned and documented the script 'PlatformDestroyer'

--- Sunset Chaser/Assets/Scripts/PlatformDestroyer.cs
+++ Sunset Chaser/Assets/Scripts/PlatformDestroyer.cs
@@ -2,20 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+Will destroy the platforms unneeded after the player has past them.
+To be used in conjuction with the point at which the platform should be destroyed.
+*/
 public class PlatformDestroyer : MonoBehaviour {
 
 	public GameObject platformDestructionPoint;
 
 	// Use this for initialization
 	void Start () {
+
+		//will find the point the platform will be destroyed
 		platformDestructionPoint = GameObject.Find("PlatformDestructionPoint");
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+
+		/*
+		When the platform point goes before the dynamically moving point of destruction
+		it will be deactivated, to be used with the ObjectPooler script, and will be 
+		actived when it will be needed again after generating it to it's new further position.
+		*/
 		if (transform.position.x < platformDestructionPoint.transform.position.x) {
-			//Destroy(gameObject);
 			gameObject.SetActive(false);
 		}
 	}
