7ae97eb7063a520ca4d70b4db893a672835c86c7
mahchuchu <matthew.hunter08@gmail.com>
2017-11-26 02:34:20 -0800

    cleaned and documented the script 'GameManager'

--- Sunset Chaser/Assets/Scripts/GameManager.cs
+++ Sunset Chaser/Assets/Scripts/GameManager.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+Manages a global variable and functions to be used in other scripts.
+Using a singleton pattern.
+*/
 public class GameManager : MonoBehaviour {
 
 	public Transform platformGenerator;
@@ -18,11 +22,20 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+		//sets the original start positions of the player and platforms
 		platformStartPoint = platformGenerator.position;
 		playerStartPoint = thePlayer.transform.position;
+
+		//variable sets to parameters from ScoreManager script
 		theScoreManager = FindObjectOfType<ScoreManager>();
 	}
 
+	/*
+	When the player dies the score will stop increasing,
+	the player will be inactive,
+	and the death menu will pop up.
+	*/
 	public void RestartGame ()
 	{
 		theScoreManager.scoreIncreasing = false;
@@ -30,17 +43,28 @@ public class GameManager : MonoBehaviour {
 		theDeathScreen.gameObject.SetActive(true);
 	}
 
+
+	//Purpose is to reset the game to be able to play again when in the Pause Menu.
 	public void Reset ()
 	{
+		//Death Menu will disappear
 		theDeathScreen.gameObject.SetActive(false);
+
+		//Deactives all the platforms
 		platformList = FindObjectsOfType<PlatformDestroyer> ();
-		for (int i = 0; i < platformList.Length; i++) {
+		for (int i = 0; i < platformList.Length; i++) 
+		{
 			platformList [i].gameObject.SetActive (false);
 		}
 
+		//The player and platforms return to original start position
 		thePlayer.transform.position = playerStartPoint;
 		platformGenerator.position = platformStartPoint;
+
+		//Activates the player
 		thePlayer.gameObject.SetActive (true); 
+
+		//resets score values
 		theScoreManager.scoreCount = 0;
 		theScoreManager.scoreIncreasing = true;
 	}
