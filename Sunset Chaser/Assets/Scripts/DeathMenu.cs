a9f66ceab7b5160508cfe43902e275b1baf1bc76
mahchuchu <matthew.hunter08@gmail.com>
2017-11-26 00:33:02 -0800

    cleaned and documented the script 'DeathMenu'

--- Sunset Chaser/Assets/Scripts/DeathMenu.cs
+++ Sunset Chaser/Assets/Scripts/DeathMenu.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//Functions for a Death Menu screen for after the player dies.
 public class DeathMenu : MonoBehaviour {
 
 	public string mainMenuLevel;
 
+	//Calls the GameManager's Reset function.
 	public void Restart () {
 		FindObjectOfType<GameManager>().Reset();
 	}
 	
-
+	//Calls the GameManager's function to bring you to the Main Menu screen
 	public void QuitToMainMenu () {
 		SceneManager.LoadScene(mainMenuLevel);
 	}
