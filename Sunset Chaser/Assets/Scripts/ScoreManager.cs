632844f5ad15ca842344afec5f6c1e8005cc0c4f
mahchuchu <matthew.hunter08@gmail.com>
2017-11-26 02:14:01 -0800

    cleaned and documented the script 'ScoreManager'

--- Sunset Chaser/Assets/Scripts/ScoreManager.cs
+++ Sunset Chaser/Assets/Scripts/ScoreManager.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/*
+This script will manage all the scoring functions for the game. 
+This includes a current score, a high score, and how they will be updated.
+*/
 public class ScoreManager : MonoBehaviour {
 
 	public Text scoreText;
@@ -17,25 +21,31 @@ public class ScoreManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		if (PlayerPrefs.HasKey("HighScore")){
+
+		if (PlayerPrefs.HasKey("HighScore"))
+		{
 			highScoreCount = PlayerPrefs.GetFloat("HighScore");
 		}
 	}
 	
 	// Update is called once per frame
-	void Update ()
-	{	
+	void Update () {	
 
+		//increases and updates the score based on how long the player can survive 
 		if (scoreIncreasing) {
 			scoreCount += pointsPerSecond * Time.deltaTime;
 		}
 
+		/*
+		if the score obtained becomes higher thand the previous high score
+		then the new high score will be updated.
+		*/
 		if (scoreCount > highScoreCount) {
 			highScoreCount = scoreCount;
 			PlayerPrefs.SetFloat("HighScore", highScoreCount);
 		}
 		 
-
+		//the score will be shown on the screen as rounded whole numbers
 		scoreText.text = "Score: " + Mathf.Round(scoreCount);
 		highScoreText.text = "High Score: " + Mathf.Round(highScoreCount);
 	}
