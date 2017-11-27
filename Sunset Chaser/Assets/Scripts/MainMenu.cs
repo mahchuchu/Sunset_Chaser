eaf66ab9a5d4143d825394fd2dbb5f1cc0df58f6
mahchuchu <matthew.hunter08@gmail.com>
2017-11-26 16:03:12 -0800

    added How to Play button on Main Menu which goes to a new scene 'Instructions' to explain the game

--- Sunset Chaser/Assets/Scripts/MainMenu.cs
+++ Sunset Chaser/Assets/Scripts/MainMenu.cs
@@ -7,6 +7,8 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour {
 
 	public string sunsetChaserGame;
+	public string instructions;
+	public string menu;
 
 	//Allows the player to start the actual Sunset Chaser game.
 	public void PlayGame () {
@@ -17,4 +19,15 @@ public class MainMenu : MonoBehaviour {
 	public void QuitGame () {
 		Application.Quit ();
 	}
+
+	//Goes to scene that explains the game and how to play it.
+	public void howToPlay () {
+		SceneManager.LoadScene (instructions);
+	}
+
+	//returns back to the main menu scene
+	public void backToMain () {
+		SceneManager.LoadScene (menu);
+	}
+
 }
